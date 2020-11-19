b698a50caf0713a69125cb25e4f400d291f9c699
roycemoses <royce.kmoses@gmail.com>
2020-11-19 14:37:03 -0800

    Declared potential members of the Entity class. Not a full-fledged declaration! Using Entity for testing.

--- header/Entity.h
+++ header/Entity.h
@@ -1,8 +1,19 @@
 #ifndef __ENTITY_H__
 #define __ENTITY_H__
 
+#include <string>
+
 class Entity 
 {
+    private:
+        std::string name;
+        int health;
+
+    public:
+        Entity();
+        Entity(std::string name, int health) : name(name), health(health) {};
+        virtual std::string getName() {};
+        virtual int getHealth() {};
 
 };
 
