84e315df07a875a8ec0cf3d104cca7a7c2f405e1
Danz Moses <danzmoses@gmail.com>
2020-11-20 14:44:29 -0800

    Fixed preliminary implementation of attack() for EnemyAttackStrategy - Implemented default constructor as well

--- header/battle/EnemyAttackStrategy.h
+++ header/battle/EnemyAttackStrategy.h
@@ -1,12 +1,17 @@
+#ifndef ENEMY_ATTACK_STRATEGY_H
+#define ENEMY_ATTACK_STRATEGY_H
+
 #include "AttackStrategy.h"
 
 class EnemyAttackStrategy : public AttackStrategy
 {
     private:
     public:
-        EnemyAttackStrategy();
+        EnemyAttackStrategy() {};
         void attack(Entity* attacker, Entity* defender)
         {
-            defender->setHP(attacker->getATK() - defender->getDEF());
+            defender->setHP(defender->getHP() - (attacker->getATK() - defender->getDEF()));
         }
 };
+
+#endif // ENEMY_ATTACK_STRATEGY_H
