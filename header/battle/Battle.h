8deed471ccfcec32ad0c388f3995d2a9022767d2
Danz Moses <danzmoses@gmail.com>
2020-11-19 20:26:30 -0800

    Fake implementation for getRoll() function in Battle class

--- header/battle/Battle.h
+++ header/battle/Battle.h
@@ -15,8 +15,23 @@ class Battle
         Entity* attacker;
         Entity* defender;
         AttackStrategy* attackStrategy;
+        void getRoll()
+        {
+            srand(time(0));
+            if (std::rand() % 2 == 0)
+            {
+                attackStrategy = new PlayerAttackStrategy();
+                attacker = player;
+                defender = enemy;
+            }
+            else
+            {
+                attackStrategy = new EnemyAttackStrategy();
+                attacker = enemy;
+                defender = player;
+            } 
+        }
 
     public:
         Battle();
-        void getRoll();  
 };
