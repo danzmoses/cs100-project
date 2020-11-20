5ca8d0c1ca239214f028fb95b8a374d2d9c87a8a
Danz Moses <danzmoses@gmail.com>
2020-11-19 19:23:18 -0800

    Class declaration for Battle class

---
+++ header/battle/Battle.h
@@ -0,0 +1,21 @@
+#include "../Player.h"
+#include "../Enemy.h"
+#include <random>
+#include <time.h>
+#include "AttackStrategy.h"
+#include "PlayerAttackStrategy.h"
+#include "EnemyAttackStrategy.h"
+
+
+class Battle
+{
+    private:
+        Player* player;
+        Enemy* enemy;
+        Entity* attacker;
+        Entity* defender;
+        AttackStrategy* attack_strategy;
+
+    public:
+        Battle();      
+};
