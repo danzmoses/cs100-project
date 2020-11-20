0abcd5685c5be962ef1c7d331aeed060b2fcfabe
roycemoses <royce.kmoses@gmail.com>
2020-11-19 19:55:14 -0800

    Implemented Default Constructor for EnemyPrototype

--- header/EnemyFactory.h
+++ header/EnemyFactory.h
@@ -4,20 +4,37 @@
 #include "EntityFactory.h"
 #include "prototypes/enemies/EnemyPrototype.h"
 #include "prototypes/enemies/GoblinPrototype.h"
+#include "prototypes/enemies/GreenSlimePrototype.h"
+#include "prototypes/enemies/RedSlimePrototype.h"
+#include <vector>
 
 class EnemyFactory : public EntityFactory
 {
     private:
         EnemyPrototype* enemy;
+        std::vector<EnemyPrototype*> enemyPrototypes;
+
     public:
-        EnemyFactory() : EntityFactory() {};
+        EnemyFactory() : EntityFactory() 
+        {
+            this->enemyPrototypes.push_back(new GoblinPrototype());
+            this->enemyPrototypes.push_back(new GreenSlimePrototype());
+            this->enemyPrototypes.push_back(new RedSlimePrototype());
+        }
         virtual Entity* createEntity(std::string name) 
         { 
-            this->enemy = new GoblinPrototype(); 
-            enemy->Clone();
-            return enemy;
-        }; // search through dict: then set the prototype if it exists: then call clone on the prototype
-
+            for (int i = 0; i < enemyPrototypes.size(); ++i)
+            {
+                if (enemyPrototypes.at(i)->getName() == name)
+                {
+                    enemy = enemyPrototypes.at(i);
+                    enemy->Clone();
+                    return enemy;
+                }
+            }
+            std::invalid_argument ia("Invalid argument. Unable to find \"" + name + '\"' );
+            throw ia;
+        }; // search through dict: then set the prototype if it exists: then call clone on the prototype and return. throw ia if unable to find
 };
 
 #endif // __ENEMY_FACTORY__
