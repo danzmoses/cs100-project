8fbe029176849d6a1c0ad94d94a308c8f0b5486a
Danz Moses <danzmoses@gmail.com>
2020-11-20 14:26:43 -0800

    Added constructor and several variables for Battle class - Constructor takes in two Entity ptrs and sets them to player and enemy - Added Entity* attacker to private variables - Added Entity* defender to private variables - Added bool playerWonRoll to private variables - Added getter for playerWonRoll

--- header/battle/Battle.h
+++ header/battle/Battle.h
@@ -1,42 +1,44 @@
-#include "../Player.h"
-#include "../Enemy.h"
+#include "../Entity.h"
 #include <random>
 #include <time.h>
 #include "AttackStrategy.h"
 #include "PlayerAttackStrategy.h"
 #include "EnemyAttackStrategy.h"
 
-
 class Battle
 {
     private:
-        Player* player;
-        Enemy* enemy;
+        Entity* player;
+        Entity* enemy;
         Entity* attacker;
         Entity* defender;
         AttackStrategy* attackStrategy;
+        bool playerWonRoll;
         void getRoll()
         {
-            srand(time(0));
             if (std::rand() % 2 == 0)
             {
                 attackStrategy = new PlayerAttackStrategy();
                 attacker = player;
                 defender = enemy;
+                playerWonRoll = true;
             }
             else
             {
                 attackStrategy = new EnemyAttackStrategy();
                 attacker = enemy;
                 defender = player;
+                playerWonRoll = false;
             } 
         }
 
     public:
         Battle();
+        Battle(Entity* player, Entity* enemy): player(player), enemy(enemy) {};
         void nextTurn()
         {
             getRoll();
             attackStrategy->attack(attacker, defender);
         }
+        bool getPlayerWonRoll() { return playerWonRoll; }
 };
