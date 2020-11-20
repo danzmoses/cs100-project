bd8038a30c8a4e3f6e1d5b0d5d6d7df95579d370
roycemoses <royce.kmoses@gmail.com>
2020-11-19 17:40:31 -0800

    Added EnemyPrototype reference and began implementing createEntity()

--- header/EnemyFactory.h
+++ header/EnemyFactory.h
@@ -2,12 +2,21 @@
 #define __ENEMY_FACTORY__
 
 #include "EntityFactory.h"
+#include "prototypes/enemies/EnemyPrototype.h"
+#include "prototypes/enemies/GoblinPrototype.h"
 
 class EnemyFactory : public EntityFactory
 {
+    private:
+        EnemyPrototype* enemy;
     public:
         EnemyFactory() : EntityFactory() {};
-        virtual Entity* createEntity(std::string name) { };
+        virtual Entity* createEntity(std::string name) 
+        { 
+            this->enemy = new GoblinPrototype(); 
+            enemy->Clone();
+            return enemy;
+        }; // search through dict: then set the prototype if it exists: then call clone on the prototype
 
 };
 
