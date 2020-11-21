0729104de1c9e115890c4ac49fc062352bd6137d
Danz Moses <danzmoses@gmail.com>
2020-11-21 15:07:39 -0800

    Updated Battle class to use Player/Enemy instead of Entity - Private function setAttackStrategy() declared and implemented - Player* and Enemy* for player and enemy variables respectively - playerRoll and enemyRoll variables and getters defined - attackStrategy design pattern redefined - getRoll() updated with appropriate attackStrategy strategies - damageDone variable and getter defined

--- header/battle/Battle.h
+++ header/battle/Battle.h
@@ -1,44 +1,66 @@
 #include "../Entity.h"
-#include <random>
-#include <time.h>
+#include "../Player.h"
+#include "../Enemy.h"
 #include "AttackStrategy.h"
-#include "PlayerAttackStrategy.h"
-#include "EnemyAttackStrategy.h"
+#include "LightAttackStrategy.h"
+#include "NormalAttackStrategy.h"
+#include "HeavyAttackStrategy.h"
+
+#include <random>
 
 class Battle
 {
     private:
-        Entity* player;
-        Entity* enemy;
+        Player* player;
+        Enemy* enemy;
         Entity* attacker;
         Entity* defender;
         AttackStrategy* attackStrategy;
+        int damageDone;
+        int playerRoll;
+        int enemyRoll;
         bool playerWonRoll;
         void getRoll()
         {
-            if (std::rand() % 2 == 0)
+            playerRoll = std::rand() % 10 + 1;
+            enemyRoll = std::rand() % 10 + 1;
+            if (playerRoll >= enemyRoll)
             {
-                attackStrategy = new PlayerAttackStrategy();
+                
                 attacker = player;
                 defender = enemy;
                 playerWonRoll = true;
+                setAttackStrategy(playerRoll - enemyRoll);
             }
             else
             {
-                attackStrategy = new EnemyAttackStrategy();
                 attacker = enemy;
                 defender = player;
                 playerWonRoll = false;
-            } 
+                setAttackStrategy(enemyRoll - playerRoll);
+            }
+        }
+        void setAttackStrategy(int diffBetweenRolls)
+        {
+            if (diffBetweenRolls >= 7)
+                attackStrategy = new HeavyAttackStrategy();
+            else if (diffBetweenRolls >= 4)
+                attackStrategy = new NormalAttackStrategy();
+            else
+                attackStrategy = new LightAttackStrategy();
         }
 
     public:
         Battle();
-        Battle(Entity* player, Entity* enemy): player(player), enemy(enemy) {};
+        Battle(Player* player, Enemy* enemy): player(player), enemy(enemy) {};
         void nextTurn()
         {
             getRoll();
-            attackStrategy->attack(attacker, defender);
+            damageDone = attackStrategy->attack(attacker, defender);
+            defender->setHP(defender->getHP() - damageDone);
         }
+        int getPlayerRoll() { return playerRoll; }
+        int getEnemyRoll() { return enemyRoll; }
+        int getDamageDone() { return damageDone; }
         bool getPlayerWonRoll() { return playerWonRoll; }
 };
