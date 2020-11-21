0e060633a67aab3be1993b74ffd7598124d15e37
roycemoses <royce.kmoses@gmail.com>
2020-11-21 15:28:18 -0800

    Removed level attribute from Entity

--- header/Entity.h
+++ header/Entity.h
@@ -10,28 +10,25 @@ class Entity
         std::string name;
         int HP;
         int maxHP;
-        int level;
         int ATK;
         int DEF;
 
     public:
-        Entity() : HP(10), maxHP(10), level(1), ATK(1), DEF(1) {}
-        Entity(std::string name) : name(name), HP(10), maxHP(10), level(1), ATK(1), DEF(1) {}
+        Entity() : HP(10), maxHP(10), ATK(1), DEF(1) {}
+        Entity(std::string name) : name(name), HP(10), maxHP(10), ATK(1), DEF(1) {}
         
         std::string getName() { return this->name; }
         int getHP() { return this->HP; }
         int getMaxHP() { return this->maxHP; }
-        int getLevel() { return this->level; }
         int getATK() { return this->ATK; }
         int getDEF() { return this->DEF; }
 
         void setName(std::string name) { this->name = name; }
         void setHP(int HP) { this->HP = HP; }
         void setMaxHP(int maxHP) { this->maxHP = maxHP; }
-        void setLevel(int level) { this->level = level; }
         void setATK(int ATK) { this->ATK = ATK; }
         void setDEF(int DEF) { this->DEF = DEF; }
-        
+        void restoreHP() { this->HP = this->maxHP; }
 };
 
 #endif // __ENTITY__
