54078dfc7a7816996e88ab6d3ad737e6506fde3d
roycemoses <royce.kmoses@gmail.com>
2020-11-21 13:33:35 -0800

    Reimplemented the features from old Entity class into Player class and added implementation for maxHP

--- header/Player.h
+++ header/Player.h
@@ -3,9 +3,26 @@
 
 class Player
 {
+    private:
+        std::string name;
+        int health;
+        int maxHP;
+        int gold;
+
     public:
         Player() {}
         Player(std::string name) {}
+        
+        std::string getName() { return this->name; }
+        int getHealth() { return this->health; }
+        int getMaxHP() { return this->maxHP; }
+        int getGold() { return this->gold; }
+
+
+        void setName(std::string name) { this->name = name; }
+        void setHealth(int health) { this->health = health; }
+        void setGold(int gold) { this->gold = gold; }
+        void setMaxHP(int maxHP) { this->maxHP = maxHP; }
 };
 
 #endif // __PLAYER_FACTORY__
