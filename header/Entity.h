fc8068525dbf0758025ccdab48deb00046bb59a8
roycemoses <royce.kmoses@gmail.com>
2020-11-21 13:09:16 -0800

    Added gold feature, max HP and getter/setter functions

--- header/Entity.h
+++ header/Entity.h
@@ -8,16 +8,23 @@ class Entity
     private:
         std::string name;
         int health;
+        int maxHP;
+        int gold;
+
 
     public:
-        Entity() {};
-        Entity(std::string name) : name(name) {};
+        Entity() {}
+        Entity(std::string name) : name(name) {}
     
-        virtual std::string getName() { return this->name; };
-        virtual int getHealth() { return this->health; };
+        virtual std::string getName() { return this->name; }
+        virtual int getHealth() { return this->health; }
 
-        virtual void setName(std::string name) { this->name = name; };
-        virtual void setHealth(int health) { this->health = health; };
+        virtual void setName(std::string name) { this->name = name; }
+        virtual void setHealth(int health) { this->health = health; }
+
+        virtual int getGold() { return this->gold; };
+        virtual void setGold(int gold) { this->gold = gold; } ;
+
 };
 
 #endif // __ENTITY_H__
