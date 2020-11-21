1043dec241c0bdde1a89209385f3740a8119f66a
roycemoses <royce.kmoses@gmail.com>
2020-11-21 14:54:54 -0800

    Implemented get and set Gold. Updated interface

--- header/Player.h
+++ header/Player.h
@@ -1,28 +1,22 @@
 #ifndef __PLAYER__
 #define __PLAYER__
 
-class Player
+#include <string>
+#include "Entity.h"
+
+class Player : public Entity
 {
     private:
-        std::string name;
-        int health;
-        int maxHP;
         int gold;
 
     public:
-        Player() {}
-        Player(std::string name) {}
+        Player() : Entity() {}
+        Player(std::string name) : Entity(name), gold(50) {}
         
-        std::string getName() { return this->name; }
-        int getHealth() { return this->health; }
-        int getMaxHP() { return this->maxHP; }
         int getGold() { return this->gold; }
 
-
-        void setName(std::string name) { this->name = name; }
-        void setHealth(int health) { this->health = health; }
         void setGold(int gold) { this->gold = gold; }
-        void setMaxHP(int maxHP) { this->maxHP = maxHP; }
+
 };
 
 #endif // __PLAYER_FACTORY__
