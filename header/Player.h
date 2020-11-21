e2e1656a44361e1f714c4858f5ad14c5cb2df48b
roycemoses <royce.kmoses@gmail.com>
2020-11-21 15:07:15 -0800

    Added EXP as a member. Implemented getEXP and setEXP

--- header/Player.h
+++ header/Player.h
@@ -8,15 +8,17 @@ class Player : public Entity
 {
     private:
         int gold;
+        int EXP;
 
     public:
-        Player() : Entity(), gold(50) {}
-        Player(std::string name) : Entity(name), gold(50) {}
+        Player() : Entity(), gold(50), EXP(0) {}
+        Player(std::string name) : Entity(name), gold(50), EXP(0) {}
         
         int getGold() { return this->gold; }
+        int getEXP() { return this->EXP; }
 
         void setGold(int gold) { this->gold = gold; }
-
+        void setEXP(int EXP) { this->EXP = EXP; }
 };
 
 #endif // __PLAYER__
