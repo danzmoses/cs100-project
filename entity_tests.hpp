1e24faa858027e2a51d0b94362b259d12e4386b3
roycemoses <royce.kmoses@gmail.com>
2020-11-22 14:37:34 -0800

    Added more to DefaultConstructorTest

--- entity_tests.hpp
+++ entity_tests.hpp
@@ -9,7 +9,14 @@ TEST(PlayerTests, DefaultConstructorTest)
 {
     Player* player = new Player();
 
+    EXPECT_EQ(player->getATK(), 1);
+    EXPECT_EQ(player->getDEF(), 1);
+    EXPECT_EQ(player->getEXP(), 0);
+    EXPECT_EQ(player->getGold(), 50);
     EXPECT_EQ(player->getHP(), 10);
+    EXPECT_EQ(player->getLevel(), 1);
+    EXPECT_EQ(player->getMaxHP(), 10);
+    EXPECT_EQ(player->getName(), "Entity");
 }
 
 #endif // __ENTITY_TESTS__
