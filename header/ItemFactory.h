793357956179c7207c2e255990ea35623287a052
roycemoses <royce.kmoses@gmail.com>
2020-11-23 11:49:32 -0800

    Implemented ItemFactory

---
+++ header/ItemFactory.h
@@ -0,0 +1,12 @@
+#ifndef __ITEM_FACTORY_H__
+#define __ITEM_FACTORY_H__
+#include "Item.h"
+
+class ItemFactory
+{
+	public:
+		ItemFactory() {};
+		virtual Item* createItem(std::string name) = 0;
+};
+
+#endif //__ITEM_FACTORY_H__
