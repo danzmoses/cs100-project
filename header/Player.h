cb14036bf4f04c83bb25f079bf9ed10246b1096c
lly022 <lly022@ucr.edu>
2020-11-22 16:02:58 -0800

    Update Player.h getters and setters

--- header/Player.h
+++ header/Player.h
@@ -13,6 +13,30 @@ class Player : public Entity{
 
 	Player(std::string name) : Entity(name), gold(50), level(1), EXP(0){}
 
+	//getters and setters
+	int getGold(){
+		return this->gold;
+	}
+
+	int getLevel(){
+		return this->level;
+	}
+
+	int getEXP(){
+		return this->EXP;
+	}
+
+	void setGold(int gold){
+		this->gold = gold;
+	}
+
+	void setLevel(int level){
+		this->level = level;
+	}
+
+	void setEXP(int EXP){
+		this->EXP = EXP;
+	}
 };
 
 #endif //__PLAYER_H__
