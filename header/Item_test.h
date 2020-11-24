4a3775e2f53db8a1610fe7bbea08bed8c37617eb
lly022 <lly022@ucr.edu>
2020-11-23 16:36:40 -0800

    Starting Item tests

---
+++ header/Item_test.h
@@ -0,0 +1,13 @@
+#ifndef __ITEM_TEST_H__
+#define __ITEM_TEST_H__
+
+#include "gtest/gtest.h"
+#include "Item.h"
+#include "ItemFactory.h"
+#include "WeaponFactory.h"
+#include "ArmorFactory.h"
+
+TEST(){
+}
+
+#endif //__ITEM_TEST_H
