3429ea66c9b1e469bf6026f6148675b0d8a974d4
roycemoses <47472265+roycemoses@users.noreply.github.com>
2020-11-23 21:32:44 -0800

    Delete test.cpp inside header/

--- header/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "gtest/gtest.h"
-#include "Entity_test.h"
-#include "Item_test.h"
-
-int main(int argc, char **argv){
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-} 
