f9018e4691d5b9a3176740cdc63f956d89c914d4
lly022 <56405110+lly022@users.noreply.github.com>
2020-11-23 21:26:28 -0800

    Update test.cpp
    Add tests

--- test.cpp
+++ test.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 
 #include "entity_tests.hpp"
+#include "Entity_test.h"
+#include "Item_test.h"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
