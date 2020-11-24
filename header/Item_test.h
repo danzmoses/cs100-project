63d5f3db300d3375722ef93010bd5021fc17ad85
lly022 <lly022@ucr.edu>
2020-11-23 16:59:25 -0800

    Add Iron Armor test, update test.cpp to include item tests

--- header/Item_test.h
+++ header/Item_test.h
@@ -3,11 +3,69 @@
 
 #include "gtest/gtest.h"
 #include "Item.h"
-#include "ItemFactory.h"
 #include "WeaponFactory.h"
 #include "ArmorFactory.h"
 
-TEST(){
+TEST(ItemTests, LeatherArmorTest){
+	ItemFactory *AF = new ArmorFactory();
+	Item *a = AF->createItem("Leather Armor");
+
+	EXPECT_EQ(a->getName(), "Leather Armor");
+	EXPECT_EQ(a->getCost(), 50); 
+	EXPECT_EQ(a->getATK(), 0);
+	EXPECT_EQ(a->getDEF(), 0);
+
+	a->setName("New Leather Armor");
+	a->setCost(200);
+	a->setATK(2);
+	a->setDEF(200);
+	
+	EXPECT_EQ(a->getName(), "New Leather Armor");
+	EXPECT_EQ(a->getCost(), 200);
+	EXPECT_EQ(a->getATK(), 2);
+	EXPECT_EQ(a->getDEF(), 200);
 }
+
+TEST(ItemTests, IronArmorTest){
+	ItemFactory *AF = new ArmorFactory();
+	Item *a = AF->createItem("Iron Armor");
+	
+	EXPECT_EQ(a->getName(), "Iron Armor");
+	EXPECT_EQ(a->getCost(), 450);
+	EXPECT_EQ(a->getATK(), 0);
+	EXPECT_EQ(a->getDEF(), 0);
+
+	a->setName("New Iron Armor");
+	a->setCost(1000);
+	a->setATK(5);
+	a->setDEF(500);
+	
+	EXPECT_EQ(a->getName(), "New Iron Armor");
+	EXPECT_EQ(a->getCost(), 1000);
+	EXPECT_EQ(a->getATK(), 5);
+	EXPECT_EQ(a->getDEF(), 500)
+}
+
+TEST(ItemTests, WoodenSwordWeaponTest){
+	ItemFactory *WF = new WeaponFactory();
+	Item *w = WF->createItem("Wooden Sword");
+	
+	EXPECT_EQ(w->getName(), "Wooden Sword");
+	EXPECT_EQ(w->getCost(), 25);
+	EXPECT_EQ(w->getATK(), 0);
+	EXPECT_EQ(w->getDEF(), 0);
+
+	w->setName("New Wooden Sword");
+	w->setCost(50);
+	w->setATK(10);
+	w->setDEF(2);
+
+	EXPECT_EQ(w->getName(), "New Wooden Sword");
+	EXPECT_EQ(w->getCost(), 50);
+	EXPECT_EQ(w->getATK(), 10);
+	EXPECT_EQ(w->getDEF(), 2);
+}
+
+
 
 #endif //__ITEM_TEST_H
--- header/test.cpp
+++ header/test.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "Entity_test.h"
+#include "Item_test.h"
 
 int main(int argc, char **argv){
 	::testing::InitGoogleTest(&argc, argv);
