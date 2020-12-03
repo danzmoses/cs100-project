c716e3d06a98231247a25b046f2874c350992b53
roycemoses <royce.kmoses@gmail.com>
2020-12-02 18:53:20 -0800

    Added RedSlimePrototypeTest

--- header/prototypes/prototype_tests.h
+++ header/prototypes/prototype_tests.h
@@ -167,4 +167,32 @@ TEST(PrototypeTests, GreenSlimePrototypeTest)
     
 }
 
+TEST(PrototypeTests, RedSlimePrototypeTest)
+{
+    EnemyFactory* ef = new EnemyFactory();
+    Enemy* enemy = ef->createEnemy("Red Slime");
+
+    EXPECT_EQ(enemy->baseStats->ATK, 1);
+    EXPECT_EQ(enemy->baseStats->DEF, 1);
+    EXPECT_EQ(enemy->baseStats->HP, 3);
+    EXPECT_EQ(enemy->baseStats->maxHP, 3);
+    EXPECT_EQ(enemy->combatStats->ATK, 1);
+    EXPECT_EQ(enemy->combatStats->DEF, 1);
+    EXPECT_EQ(enemy->combatStats->HP, 3);
+    EXPECT_EQ(enemy->combatStats->maxHP, 3);
+    
+    EXPECT_EQ(enemy->getName(), "Red Slime");
+    EXPECT_EQ(enemy->getLevel(), 1);
+    EXPECT_EQ(enemy->getDescription(), "A bouncing blob of goo. It looks aggressive.");
+
+    enemy->setName("Mega Goblin");
+    enemy->setLevel(2);
+    enemy->setDescription("test desc.");
+
+    EXPECT_EQ(enemy->getName(), "Mega Goblin");
+    EXPECT_EQ(enemy->getLevel(), 2);
+    EXPECT_EQ(enemy->getDescription(), "test desc.");
+    
+}
+
 #endif // __PROTOTYPE_TESTS_H__
