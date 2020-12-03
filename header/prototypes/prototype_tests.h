c28e8e39ce8725e11a7c8161714bcd83b5dc6ed0
roycemoses <royce.kmoses@gmail.com>
2020-12-02 18:01:47 -0800

    Initial commit for prototype_tests.h

---
+++ header/prototypes/prototype_tests.h
@@ -0,0 +1,11 @@
+#ifndef __PROTOTYPE_TESTS_H__
+#define __PROTOTYPE_TESTS_H__
+
+#include "../header/Player.h"
+#include "../header/EnemyFactory.h"
+#include "../header/ArmorFactory.h"
+#include "../header/WeaponFactory.h"
+
+
+
+#endif // __PROTOTYPE_TESTS_H__
