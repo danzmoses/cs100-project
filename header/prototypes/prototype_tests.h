4d05edf53a32949846d58bfeebcc72610a2e93bd
roycemoses <royce.kmoses@gmail.com>
2020-12-02 18:23:30 -0800

    Added LeatherArmorPrototypeTest

--- header/prototypes/prototype_tests.h
+++ header/prototypes/prototype_tests.h
@@ -10,26 +10,52 @@
 TEST(PrototypeTests, IronArmorPrototypeTest)
 {
     ItemFactory* af = new ArmorFactory();
-    Item* ironArmor = af->createItem("Iron Armor");
+    Item* armor = af->createItem("Iron Armor");
 
 
-    EXPECT_EQ(ironArmor->baseStats->ATK, 0);
-    EXPECT_EQ(ironArmor->baseStats->DEF, 2);
-    EXPECT_EQ(ironArmor->baseStats->HP, 0);
-    EXPECT_EQ(ironArmor->baseStats->maxHP, 0);
-    EXPECT_EQ(ironArmor->combatStats->ATK, 0);
-    EXPECT_EQ(ironArmor->combatStats->DEF, 2);
-    EXPECT_EQ(ironArmor->combatStats->HP, 0);
-    EXPECT_EQ(ironArmor->combatStats->maxHP, 0);
+    EXPECT_EQ(armor->baseStats->ATK, 0);
+    EXPECT_EQ(armor->baseStats->DEF, 2);
+    EXPECT_EQ(armor->baseStats->HP, 0);
+    EXPECT_EQ(armor->baseStats->maxHP, 0);
+    EXPECT_EQ(armor->combatStats->ATK, 0);
+    EXPECT_EQ(armor->combatStats->DEF, 2);
+    EXPECT_EQ(armor->combatStats->HP, 0);
+    EXPECT_EQ(armor->combatStats->maxHP, 0);
     
-    EXPECT_EQ(ironArmor->getCost(), 450);
-    EXPECT_EQ(ironArmor->getName(), "Iron Armor");
+    EXPECT_EQ(armor->getCost(), 450);
+    EXPECT_EQ(armor->getName(), "Iron Armor");
 
-    ironArmor->setCost(9999);
-    ironArmor->setName("Super cool Iron Armor");
+    armor->setCost(9999);
+    armor->setName("Super cool Iron Armor");
 
-    EXPECT_EQ(ironArmor->getCost(), 9999);
-    EXPECT_EQ(ironArmor->getName(), "Super cool Iron Armor");
+    EXPECT_EQ(armor->getCost(), 9999);
+    EXPECT_EQ(armor->getName(), "Super cool Iron Armor");
+    
+}
+
+TEST(PrototypeTests, LeatherArmorPrototypeTest)
+{
+    ItemFactory* af = new ArmorFactory();
+    Item* armor = af->createItem("Leather Armor");
+
+
+    EXPECT_EQ(armor->baseStats->ATK, 0);
+    EXPECT_EQ(armor->baseStats->DEF, 1);
+    EXPECT_EQ(armor->baseStats->HP, 0);
+    EXPECT_EQ(armor->baseStats->maxHP, 0);
+    EXPECT_EQ(armor->combatStats->ATK, 0);
+    EXPECT_EQ(armor->combatStats->DEF, 1);
+    EXPECT_EQ(armor->combatStats->HP, 0);
+    EXPECT_EQ(armor->combatStats->maxHP, 0);
+    
+    EXPECT_EQ(armor->getCost(), 50);
+    EXPECT_EQ(armor->getName(), "Leather Armor");
+
+    armor->setCost(9999);
+    armor->setName("Super cool Leather Armor");
+
+    EXPECT_EQ(armor->getCost(), 9999);
+    EXPECT_EQ(armor->getName(), "Super cool Leather Armor");
     
 }
 
