7e9d806b9f7b8af5a68236e1ce3d4f19d1c8517e
roycemoses <royce.kmoses@gmail.com>
2020-12-02 18:48:11 -0800

    Formatting in GoblinPrototype

--- header/prototypes/enemies/GoblinPrototype.h
+++ header/prototypes/enemies/GoblinPrototype.h
@@ -7,17 +7,17 @@ class GoblinPrototype : public EnemyPrototype
 {
     public:
         GoblinPrototype() : EnemyPrototype() { 
-	    setName("Goblin");
-		setLevel(1);
-		setDescription("Adventurous, greedy, and loot-heavy! Goblins tend to have a higher chance to drop items."); 
-	    baseStats->HP = 4;
-	    baseStats->maxHP = 4;
-	    baseStats->ATK = 3;
-	    baseStats->DEF = 1;
-	    combatStats->HP = 4;
-	    combatStats->maxHP = 4;
-	    combatStats->ATK = 3;
-	    combatStats->DEF = 1;
+			setName("Goblin");
+			setLevel(1);
+			setDescription("Adventurous, greedy, and loot-heavy! Goblins tend to have a higher chance to drop items."); 
+			baseStats->HP = 4;
+			baseStats->maxHP = 4;
+			baseStats->ATK = 3;
+			baseStats->DEF = 1;
+			combatStats->HP = 4;
+			combatStats->maxHP = 4;
+			combatStats->ATK = 3;
+			combatStats->DEF = 1;
         }
         GoblinPrototype(std::string name) : EnemyPrototype(name) {}
         virtual EnemyPrototype* Clone() { return new GoblinPrototype(); }
