f46ce29607a663e13d1e7ddbdb1af0f51d697aec
lly022 <lly022@ucr.edu>
2020-12-04 21:39:27 -0800

    Add constructor to EnhanceDEFPrototype

---
+++ header/prototypes/cards/EnhanceDEFPrototype.h
@@ -0,0 +1,20 @@
+#ifndef __ENHANCE_DEF_PROTOTYPE_H__
+#define __ENHANCE_DEF_PROTOTYPE_H__
+
+#include "CardPrototype.h"
+
+class EnhanceDEFPrototype : public CardPrototype{
+	public:
+		EnhanceDEFPrototype() : CardPrototype(){
+			this->setName("Enhance DEF");
+			this->setCost(75);
+			this->setDescription("This card enhances DEF by 1");
+		}
+
+		EnhanceDEFPrototype(std::string name) : CardPrototype(name){
+			this->setCost(75);
+			this->setDescription("This card enhances DEF by 1");
+		}
+};
+
+#endif //__ENHANCE_DEF_PROTOTYPE_H__
