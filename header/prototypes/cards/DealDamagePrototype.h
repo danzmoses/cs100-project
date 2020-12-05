2d6fc14e47485bdfaeb5db60624be6ac3b8dd048
lly022 <lly022@ucr.edu>
2020-12-04 21:51:24 -0800

    Add Clone function to DealDamagePrototype

--- header/prototypes/cards/DealDamagePrototype.h
+++ header/prototypes/cards/DealDamagePrototype.h
@@ -15,6 +15,10 @@ class DealDamagePrototype : public CardPrototype{
 			this->setCost(85);
 			this->setDescription("This card decreases the enemy's HP by 1");
 		}
+
+		virtual CardPrototype* Clone(){
+			return new DealDamagePrototype(); 
+		}
 };
 
 #endif //__DEAL_DAMAGE_PROTOTYPE_H__
