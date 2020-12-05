0869419d5fd2a3af74a2cd20d78d2ecb30c87a22
Danz Moses <danzmoses@gmail.com>
2020-12-04 17:55:22 -0800

    Added LightAttackStrategyEnemyNoDamage test for AttackStrategy tests

--- header/battle/attack_strategy_tests.h
+++ header/battle/attack_strategy_tests.h
@@ -99,5 +99,25 @@ TEST(AttackStrategyTests, LightAttackStrategyEnemyDealsDamage)
     EXPECT_EQ(player->combatStats->HP, 3);
 }
 
+TEST(AttackStrategyTests, LightAttackStrategyEnemyNoDamage)
+{
+    Entity* player = new Player;
+    EnemyFactory ef;
+    Entity* enemy = ef.createEnemy("Green Slime");
 
+    player->combatStats->HP = 5;        // player HP = 5
+    player->combatStats->ATK = 1;       // player ATK = 1
+    player->combatStats->DEF = 3;       // player DEF = 3
+
+    enemy->combatStats->HP = 5;         // enemy HP = 5
+    enemy->combatStats->ATK = 3;        // enemy ATK = 3
+    enemy->combatStats->DEF = 5;        // enemy DEF = 5
+
+    // LightAttackStrategy: damage_done = attacker->ATK - defender->DEF = 3 - 3 = 0 (if negative, damage = 0)
+    AttackStrategy* strategy = new LightAttackStrategy();
+    int damage_done = strategy->attack(enemy, player);
+    EXPECT_EQ(damage_done, 0);
+    player->combatStats->HP -= damage_done;
+    EXPECT_EQ(player->combatStats->HP, 5);
+}
 #endif // __ATTACK_STRATEGY_TESTS_H__
