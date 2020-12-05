e6e9abefc5936be6f39847bb3cdf02064dad2bf4
roycemoses <royce.kmoses@gmail.com>
2020-12-04 19:06:00 -0800

    Added private Inventory members. Declared and implemented getInventory() and getEquipped()

--- header/Entity.h
+++ header/Entity.h
@@ -7,7 +7,8 @@
 #include "Inventory.h"
 #include "ItemFactory.h"
 
-class Entity {
+class Entity 
+{
     private:
         std::string name;
         int level;
@@ -16,63 +17,50 @@ class Entity {
 
 
     public:
-	Stats *baseStats = new Stats();
-	Stats *combatStats = new Stats();
-    Entity(){
-        this->name = "Entity";
-        this->level = 1;
-        baseStats->HP = 10;
-        baseStats->maxHP = 10;
-        baseStats->ATK = 1;
-        baseStats->DEF = 1;
-        combatStats->HP = 10;
-        combatStats->maxHP = 10;
-        combatStats->ATK = 1;
-        combatStats->DEF = 1;
-	}
+        Stats *baseStats = new Stats();
+        Stats *combatStats = new Stats();
 
-    Entity(std::string name){
-        this->name= name;
-        this->level = 1;
-        baseStats->HP = 10;
-        baseStats->maxHP = 10;
-        baseStats->ATK = 1;
-        baseStats->DEF = 1;
-        combatStats->HP = 10;
-        combatStats->maxHP = 10;
-        combatStats->ATK = 1;
-        combatStats->DEF = 1;
-    }
-        
-    std::string getName() { return this->name; }
-    int getLevel() { return this->level; }
-    Inventory getInventory() { return this->inventory; }
-    Inventory getEquipped() { return this->equipped; }
-
-    void setName(std::string name) { this->name = name; }
-    void setLevel(int level){ this->level = level; }
-    void restoreHP() { combatStats->HP = combatStats->maxHP; }
-    void addWeaponToInventory(std::string name, ItemFactory* wf) { this->inventory.addWeapon(wf->createItem(name)); }
-    void addArmorToInventory(std::string name, ItemFactory* af) { this->inventory.addArmor(af->createItem(name)); }
-
-    void equipWeapon(std::string name)
-    {
-        for (int i = 0; i < this->inventory.getWeapons().size(); ++i)
+        Entity()
         {
-            if (this->inventory.getWeapons().at(i)->getName() == name)
-            {
-                if (this->equipped.getWeapons().size() >= 1)
-                {
-                    this->equipped.getWeapons().clear();
-                    this->equipped.getWeapons().push_back(this->inventory.getWeapons().at(i));
-                    return;
-                }
-            }
-		}
-        std::invalid_argument ia("Invalid argument. Unable to find \"" + name + "\" inside of this inventory.");
-        throw ia;
-	}
+            this->name = "Entity";
+            this->level = 1;
+            baseStats->HP = 10;
+            baseStats->maxHP = 10;
+            baseStats->ATK = 1;
+            baseStats->DEF = 1;
+            combatStats->HP = 10;
+            combatStats->maxHP = 10;
+            combatStats->ATK = 1;
+            combatStats->DEF = 1;
+        }
 
+        Entity(std::string name)
+        {
+            this->name= name;
+            this->level = 1;
+            baseStats->HP = 10;
+            baseStats->maxHP = 10;
+            baseStats->ATK = 1;
+            baseStats->DEF = 1;
+            combatStats->HP = 10;
+            combatStats->maxHP = 10;
+            combatStats->ATK = 1;
+            combatStats->DEF = 1;
+        }
+        
+        // __GETTERS__
+        std::string getName() { return this->name; }
+        int getLevel() { return this->level; }
+        Inventory getInventory() { return this->inventory; }
+        Inventory getEquipped() { return this->equipped; }
+
+        // __SETTERS__
+        void setName(std::string name) { this->name = name; }
+        void setLevel(int level){ this->level = level; }
+
+        // __PUBLIC_FUNCTIONS__
+        void restoreHP() { combatStats->HP = combatStats->maxHP; }
+        
 
 };
 
