f9e35038bbc0f29ebcdcba78c198b71dd239b893
Danz Moses <danzmoses@gmail.com>
2020-12-04 19:13:36 -0800

    Added NormalAttackStrategyPlayerDealsDamage test for AttackStrategy tests

--- header/battle/attack_strategy_tests.h
+++ header/battle/attack_strategy_tests.h
@@ -143,6 +143,26 @@ TEST(AttackStrategyTests, LightAttackStrategyEnemyNegativeDamage)
     EXPECT_EQ(player->combatStats->HP, 5);
 }
 
+TEST(AttackStrategyTests, NormalAttackStrategyPlayerDealsDamage)
+{
+    Entity* player = new Player;
+    EnemyFactory ef;
+    Entity* enemy = ef.createEnemy("Green Slime");
 
+    player->combatStats->HP = 5;        // player HP = 5
+    player->combatStats->ATK = 1;       // player ATK = 1
+    player->combatStats->DEF = 1;       // player DEF = 1
+
+    enemy->combatStats->HP = 5;         // enemy HP = 5
+    enemy->combatStats->ATK = 1;        // enemy ATK = 1
+    enemy->combatStats->DEF = 0;        // enemy DEF = 0
+
+    // LightAttackStrategy: damage_done = (attacker->ATK*2) - defender->DEF = 1*2 - 0 = 2
+    AttackStrategy* strategy = new NormalAttackStrategy();
+    int damage_done = strategy->attack(player, enemy);
+    EXPECT_EQ(damage_done, 2);
+    enemy->combatStats->HP -= damage_done;
+    EXPECT_EQ(enemy->combatStats->HP, 3);
+}
 
 #endif // __ATTACK_STRATEGY_TESTS_H__
