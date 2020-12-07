c7adbed0d7144b6fb61ae58da4464550d65efebc
lly022 <lly022@ucr.edu>
2020-12-06 23:25:27 -0800

    Improve DealDamageStrategy

--- header/battle/DealDamageStrategy.h
+++ header/battle/DealDamageStrategy.h
@@ -2,13 +2,22 @@
 #define __DEAL_DAMAGE_STRATEGY_H__
 
 #include "CardStrategy.h"
+#include <cmath>
 
 class DealDamageStrategy : public CardStrategy{
 	public:
 		DealDamageStrategy() {};
 
 		virtual void use(Entity *attacker, Entity *defender){
-			defender->combatStats->HP -= 1; 
+			//defender->combatStats->HP -= 1;
+			//defenders lose 30% of their current health, rounded to nearest number
+			double thirtyPercentHP = (3.0/10) * defender->combatStats->HP;
+                        if(defender->combatStats->HP - round(thirtyPercentHP) > 0){
+                                defender->combatStats->HP -= round(thirtyPercentHP);
+                        }
+                        else if(attacker->combatStats->HP - round(fourtyPercentHP) <= 0){
+                                defender->combatStats->HP = 0;
+                        }
 		}
 };
 
