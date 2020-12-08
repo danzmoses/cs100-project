c316e95533638b822f0af7a091d2b51cb810b0f9
Danz Moses <danzmoses@gmail.com>
2020-12-08 10:51:26 -0800

    buyWeapon() and getShopInventory() implemented in Shop class

--- header/Shop.h
+++ header/Shop.h
@@ -1,8 +1,7 @@
 #ifndef SHOP_H
 #define SHOP_H
 
-#include "Player.h"
-#include "Enemy.h"
+#include "Entity.h"
 #include "Inventory.h"
 #include "WeaponFactory.h"
 #include "ArmorFactory.h"
@@ -10,20 +9,54 @@
 class Shop
 {
     private:
-        Inventory itemsForSale;
-        WeaponFactory weaponFactory;
-        ArmorFactory armorFactory;   
+        Inventory shopInventory;
+        WeaponFactory* weaponFactory;
+        ArmorFactory* armorFactory;
+
 
     public:
         Shop()
         {
-            std::vector<Item*> weapons = weaponFactory.getPrototypes();
+            weaponFactory = new WeaponFactory;
+            armorFactory = new ArmorFactory;
+            std::vector<Item*> weapons = weaponFactory->getPrototypes();
             for (unsigned i = 0; i < weapons.size(); ++i)
-                itemsForSale.addWeapon(weapons[i]);
-            std::vector<Item*> armor = armorFactory.getPrototypes();
+                shopInventory.addWeapon(weapons[i]);
+            std::vector<Item*> armor = armorFactory->getPrototypes();
             for (unsigned i = 0; i < armor.size(); ++i)
-                itemsForSale.addArmor(armor[i]);            
+                shopInventory.addArmor(armor[i]);            
         };
+
+        // void listWeapons()
+        // {
+        //     std::vector<Item*> weapons = shopInventory.getWeapons();
+        //     for (unsigned i = 0; i < weapons.size(); ++i)
+        //         std::cout << weapons[i]->getName() << std::endl;
+        // }
+        // void listArmors()
+        // {
+        //     std::vector<Item*> armors = shopInventory.getArmor();
+        //     for (unsigned i = 0; i < armors.size(); ++i)
+        //         std::cout << armors[i]->getName() << std::endl;            
+        // }
+
+        Inventory getShopInventory() { return shopInventory; }
+        bool buyWeapon(Entity* player, std::string weapon)
+        {
+            Item* item = nullptr;
+            try {
+                item = shopInventory.getItem(weapon);
+            } catch(const std::invalid_argument &ia) {
+                return false;
+            }
+
+            if (player->getGold() < item->getCost())
+                return false;
+            player->setGold(player->getGold() - item->getCost());
+            player->addWeaponToInventory(weapon, weaponFactory);
+            shopInventory.removeItem(weapon);
+            return true;
+        }
 };
 
 #endif // SHOP_H
