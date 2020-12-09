0446177160be7b12555b74c9b55978c853e4543c
Danz Moses <danzmoses@gmail.com>
2020-12-09 01:24:58 -0800

    Shop constructor now adds one of each card prototype to its inventory

--- header/Shop.h
+++ header/Shop.h
@@ -5,6 +5,7 @@
 #include "Inventory.h"
 #include "WeaponFactory.h"
 #include "ArmorFactory.h"
+#include "CardFactory.h"
 
 class Shop
 {
@@ -12,20 +13,24 @@ class Shop
         Inventory shopInventory;
         WeaponFactory* weaponFactory;
         ArmorFactory* armorFactory;
+        CardFactory* cardFactory;
 
     public:
         Shop()
         {
             weaponFactory = new WeaponFactory;
             armorFactory = new ArmorFactory;
+            cardFactory = new CardFactory;
             std::vector<Item*> weapons = weaponFactory->getPrototypes();
             for (unsigned i = 0; i < weapons.size(); ++i)
                 shopInventory.addWeapon(weapons[i]);
             std::vector<Item*> armor = armorFactory->getPrototypes();
             for (unsigned i = 0; i < armor.size(); ++i)
                 shopInventory.addArmor(armor[i]);            
-        };
-        Inventory getShopInventory() { return shopInventory; }
+            std::vector<Card*> cards = cardFactory->getPrototypes();
+            for (unsigned i = 0; i < cards.size(); ++i)
+                shopInventory.addCard(cards[i]);
+        }
         bool buyWeapon(Entity* player, std::string weapon)
         {
             Item* item = nullptr;
@@ -57,7 +62,8 @@ class Shop
             player->addArmorToInventory(armor, armorFactory);
             shopInventory.removeItem(armor);
             return true;
-        }        
+        }
+        Inventory getShopInventory() { return shopInventory; }
 };
 
 #endif // SHOP_H
